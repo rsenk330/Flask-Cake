c1927c64de82d8fb1c4877a75e8462988b93f412
Ryan Senkbeil <rsenk330@gmail.com>
2012-01-27 18:47:53 -0600

    Added the ability to specify alternative tasks.

--- docs/index.rst
+++ docs/index.rst
@@ -43,7 +43,7 @@ Like most Flask extensions, a Flask-Cake instance may be used with multiple appl
     cake = Cake()
     cake.init_app(app)
 
-Currently, Flask-Cake requires you to have a directoy structure like this:
+Currently, Flask-Cake requires you to have a directory structure like this:
 
 .. code-block:: text
 
@@ -52,7 +52,17 @@ Currently, Flask-Cake requires you to have a directoy structure like this:
         coffee/
           Cakefile
 
-Inside the Cakefile, you need to have a ``build`` task. This will be configurable in the future, along with the path to the Cakefile. The ``build`` task can do whatever you want. Here is a simple example that combines many different `.coffee` files into a single `.js` file in a different directory:
+By default, ``cake build`` will be executed. If your task is called something else, you can specifiy that with an additional argument when you create an instance of :class:`flask_cake.Cake`. If you need to run multiple tasks, you can also specify a list of tasks:
+
+.. code-block:: python
+
+    app = Flask(__name__)
+    cake = Cake(app, "build")
+
+    # Or, specify multiple tasks
+    cake = Cake(app, ["build", "minify"])
+
+Here is a simple example of a `build` task that combines many different `.coffee` files into a single `.js` file in a different directory:
 
 .. code-block:: sh
 
--- flask_cake/cake.py
+++ flask_cake/cake.py
@@ -7,15 +7,28 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 class Cake(object):
-    def __init__(self, app=None):
-        if app is not None:
-            self.app = app
-            self.init_app(self.app)
-        else:
-            self.app = None
+    def __init__(self, app=None, tasks=["build"]):
+        """Initalize a new instance of Flask-Cake.
 
-    def init_app(self, app):
+        :param app: The Flask app
+        :param tasks: A string containing a cake "task" to execute or a list
+                      of multiple cake tasks to run. By default, this will run
+                      ``cake build``.
+
+        """
+        self.init_app(app, tasks)
+
+    def init_app(self, app, tasks):
+        """Initalize a new instance of Flask-Cake.
+
+        :param app: The Flask app
+        :param tasks: A string containing a cake "task" to execute or a list
+                      of multiple cake tasks to run. By default, this will run
+                      ``cake build``.
+
+        """
         self.app = app
+        self.tasks = tasks
 
         self._watchdog()
 
@@ -25,8 +38,6 @@ class Cake(object):
         The directory currently requires the CoffeeScript files to be located
         in `static/coffee`. This directory should contain the `Cakefile`. When
         first run, it touches the `Cakefile` to trigger the initial build.
-
-        Currently, the `Cakefile` needs to have a ``build`` command.
 
         """
         if not hasattr(self.app, 'static_url_path'):
@@ -45,7 +56,7 @@ class Cake(object):
         cakedir = os.path.join(static_dir, "coffee")
 
         # Setup Watchdog
-        handler = Events(cakedir=cakedir)
+        handler = Events(cakedir=cakedir, tasks=self.tasks)
         observer = Observer(timeout=5000)
         observer.schedule(handler, path=cakedir, recursive=True)
         observer.start()
@@ -58,16 +69,24 @@ class Cake(object):
 class Events(FileSystemEventHandler):
     """Handler for all filesystem events."""
 
-    def __init__(self, cakedir):
+    def __init__(self, cakedir, tasks):
         super(Events, self).__init__()
 
         self._cakedir = cakedir
+        self._tasks = tasks
 
     def on_any_event(self, event):
         nullfh = open(os.devnull, "w")
 
+        # Check to see if the tasks are specified as a single task or multiple
+        # tasks.
+        if isinstance(self._tasks, basestring):
+            tasks = [self._tasks]
+        else:
+            tasks = self._tasks
+
         # Run `cake build` and send all stdout to `/dev/null`.
-        p = subprocess.Popen(["cake", "build"], cwd=self._cakedir, stdout=nullfh)
+        p = subprocess.Popen(["cake"] + tasks, cwd=self._cakedir, stdout=nullfh)
         p.wait()
 
         nullfh.close()
